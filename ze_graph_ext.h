c7cb5d218ca14f6a81b3ef0bb89e718e9fcdba8e
jburcham-intel <jay.burcham@intel.com>
2026-04-21 10:43:42 -0700

    Add flag ZE_GRAPH_FLAG_SECURE_COMPILE and bump to v1.17

--- ze_graph_ext.h
+++ ze_graph_ext.h
@@ -47,7 +47,8 @@ typedef enum _ze_graph_ext_version_t
     ZE_GRAPH_EXT_VERSION_1_14 = ZE_MAKE_VERSION( 1, 14),            ///< version 1.14
     ZE_GRAPH_EXT_VERSION_1_15 = ZE_MAKE_VERSION( 1, 15),            ///< version 1.15
     ZE_GRAPH_EXT_VERSION_1_16 = ZE_MAKE_VERSION( 1, 16),            ///< version 1.16
-    ZE_GRAPH_EXT_VERSION_CURRENT = ZE_GRAPH_EXT_VERSION_1_16,       ///< latest known version
+    ZE_GRAPH_EXT_VERSION_1_17 = ZE_MAKE_VERSION( 1, 17),            ///< version 1.17
+    ZE_GRAPH_EXT_VERSION_CURRENT = ZE_GRAPH_EXT_VERSION_1_17,       ///< latest known version
     ZE_GRAPH_EXT_VERSION_FORCE_UINT32 = 0x7fffffff
 
 } ze_graph_ext_version_t;
@@ -521,6 +522,7 @@ typedef enum _ze_graph_flags_t
                                                                     ///<   1. Invalidating before destroying graph handle results in undefined behavior
                                                                     ///<   2. inputSize and pInput address must be page-aligned
                                                                     ///<   3. non-aligned values will result in ZE_RESULT_ERROR_UNSUPPORTED_ALIGNMENT
+    ZE_GRAPH_FLAG_SECURE_COMPILE = ZE_BIT(3),                       ///< Compile graph in secure mode where the driver applies additional protections to the compilation process                                                                     
 
     ZE_GRAPH_FLAG_FORCE_UINT32 = 0x7fffffff
 
